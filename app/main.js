f40b50deec26c7ebb385ae89d6090bf393d74d3c
Nickollas <nickollassilva90@gmail.com>
2024-03-27 13:22:22 -0300

    Modificando a estrutura de arquivos. Criando o método map

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 let livros = [];
-const containerLivros = document.querySelector('#livros');
+
 const endPoint = ('https://guilhermeonrails.github.io/casadocodigo/livros.json')
 
 buscarLivros();
@@ -7,22 +7,6 @@ buscarLivros();
 async function buscarLivros() {
     const res = await fetch(endPoint)
     livros = await res.json();
-    exibirLivrosNaTela(livros);
-}
-function exibirLivrosNaTela(listaDeLivros){
-    listaDeLivros.forEach(livro => {
-        containerLivros.innerHTML += `
-            <div class="livro">
-                <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-                <h2 class="livro__titulo">
-                ${livro.titulo}
-                </h2>
-                <p class="livro__descricao">${livro.autor}</p>
-                <p class="livro__preco" id="preco">R$${livro.preco}</p>
-                <div class="tags">
-                <span class="tag">${livro.categoria}</span>
-                </div>
-            </div>
-        `
-    })
+    let livrosComDesconto = aplicarDesconto(livros);
+    exibirLivrosNaTela(livrosComDesconto);
 }
